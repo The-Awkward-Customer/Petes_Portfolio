cb706d00d68a8b8370285c2415a191eb083ff3bf
The-Awkward-Customer <93682000+The-Awkward-Customer@users.noreply.github.com>
2021-11-28 22:05:00 +0000

    changes

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Peter to <a href="https://nextjs.org">Next.js!</a>
+          Welcome Peter H Abbott to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
