c62776bcb437d2a296fa13c54ba2c04d112d66e0
Peter Abbott <abbottsayshi@gmail.com>
2021-11-29 16:12:36 +0000

    Added Navbar

---
+++ Components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+            <h1>Navbar list</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Test Pages</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
