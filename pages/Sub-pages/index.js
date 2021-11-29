e051e377f82f15fd656d65f0c01cc9ad050a456a
Peter Abbott <abbottsayshi@gmail.com>
2021-11-29 15:34:36 +0000

    added routing

---
+++ pages/Sub-pages/index.js
@@ -0,0 +1,11 @@
+const Subpages = () => {
+    return (
+        <div>
+        <h1>Sub-Pages</h1>
+        <p>Hello there… this is the about page</p>
+        <p>lorem ipsum</p>
+        </div>
+    );
+}
+ 
+export default Subpages;
