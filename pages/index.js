753fb5c13f454c47dc7bb36d2ca16c06f860b4a0
Peter Abbott <abbottsayshi@gmail.com>
2021-11-29 17:00:14 +0000

    Added Links

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Footer from '../Components/Footer'
 import Navbar from '../Components/Navbar'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
