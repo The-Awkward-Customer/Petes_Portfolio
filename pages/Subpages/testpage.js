d1a30cb24ff179a16df0bb3f81249bd5eb73f9bd
Peter Abbott <abbottsayshi@gmail.com>
2021-11-29 15:38:14 +0000

    Amended page routing names

---
+++ pages/Subpages/testpage.js
@@ -0,0 +1,11 @@
+const Testpage = () => {
+    return (
+    <div>
+        <h1>Sub-Page</h1>
+        <p>Hello there… this is the about page</p>
+        <p>lorem ipsum</p>
+        </div>
+        );
+}
+ 
+export default Testpage;
